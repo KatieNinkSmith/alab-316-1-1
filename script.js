1d117db7cfb6eca3c8edd1c3f07eae2b9c553710
Katie Nink Smith <katiej.smith@outlook.com>
2024-11-05 22:52:10 -0700

    im not understanding why document.querySelectorAll('#top-menu a') wont work so i need sleep

--- script.js
+++ script.js
@@ -1,20 +1,106 @@
-
-// *****part 1
+// Menu data structure
+var menuLinks = [
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '#', subLinks: [
+    {text: 'all', href: '/catalog/all'},
+    {text: 'top selling', href: '/catalog/top'},
+    {text: 'search', href: '/catalog/search'},
+  ]},
+  {text: 'orders', href: '#' , subLinks: [
+    {text: 'new', href: '/orders/new'},
+    {text: 'pending', href: '/orders/pending'},
+    {text: 'history', href: '/orders/history'},
+  ]},
+  {text: 'account', href: '#', subLinks: [
+    {text: 'profile', href: '/account/profile'},
+    {text: 'sign out', href: '/account/signout'},
+  ]},
+];
 const mainE1 = document.querySelector("main");
+const topMenuEl = document.querySelector("#top-menu");
+// 361.1 *****part 1
+const subMenuEl = document.querySelector("#sub-menu")
+// // 361.3 *****part 4
+const topMenuLinks = document.querySelectorAll('#top-menu a');
+console.log(topMenuLinks);
+
+// 361.1 *****part 1
 mainE1.style.backgroundColor = "var(--main-bg)";
 mainE1.innerHTML = "<h1>DOM Manipulation</h1>"
 mainE1.classList.add("flex-ctr");
 
-// *****part 2
-const topMenuE1 = document.querySelector("#top-menu");
-topMenuEl.style.height = "100%";
-topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
-topMenuEl.classList.add("flex-around");
 
-// *****part 3
+// 316.1 *****part 2
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.classList.add("flex-around");
+// 361.3 *****part 4
+topMenuEl.addEventListener("click", clickedOn)
+// if (topMenuLinks.classList.contains(avtive)){
+//   menuLinks.classList.remove('active');
+//   };
+// for (const link of document.links){
+//   console.log(link);
+// }
+document.addEventListener('DOMContentLoaded', () => {
+
+  for(i=0; i<subMenuEl.length; i++){
+    const clicked = subMenuEl[i];
+      // console.log(topMenuLinks[i]);
+    clicked.addEventListener('click', function(){
+      menuLinks = document.querySelectorAll('a');
+      for (i=0; i<menuLinks.length; i++){
+        menuLinks[i].classList.remove('active');
+      }
+      this.classList.toggle('active');
+  })
+}
+});
+document.addEventListener('DOMContentLoaded', () => {
+  for(i=0; i<subMenuEl.length; i++){
+    const clicked = subMenuEl[i];
+      // console.log(topMenuLinks[i]);
+    clicked.addEventListener('click', function(){
+      menuLinks = document.querySelectorAll('a');
+      for (i=0; i<menuLinks.length; i++){
+        menuLinks[i].classList.remove('active');
+      }
+      this.classList.toggle('active');
+  })
+}
+});
+// console.log(topMenuLinks.class);
+function clickedOn(evt){
+
+  // console.log(evt.target.textContent);
+  if (evt.target.localName !== 'a'){
+    evt.preventDefault()
+    return
+  }
+  if (evt.target.localName === evt.target.localName){
+  // console.log(evt.target.textContent);
+  }
+}
+// function myFunction(e) {
+//   var elems = document.querySelector(".active");
+//   if(elems !==null){
+//    elems.classList.remove("active");
+//   }
+//  e.target.className = "active";
+// }
+// console.log(topMenuLinks.target);
+
+// 361.1 *****part 3
 menuLinks.forEach(link => {
   const newEl = document.createElement("a");
   newEl.href = link.href;
   newEl.textContent = link.text;
   topMenuEl.append(newEl);
 })
+
+// 316.3 ***** part 3
+subMenuEl.style.height = "100%";
+subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+subMenuEl.classList.add("flex-around");
+subMenuEl.style.position = "absolute";
+subMenuEl.style.top = "0";
