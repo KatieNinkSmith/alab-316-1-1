ea24830a02c08dbfda9c2c87d076a218a64bd50f
Katie Nink Smith <katiej.smith@outlook.com>
2024-11-09 17:33:14 -0700

    submenu close when topMenu isnt active fix

--- script.js
+++ script.js
@@ -31,11 +31,11 @@ var menuLinks = [
 const mainEl = document.querySelector("main");
 const topMenuEl = document.getElementById("top-menu");
 // console.log(topMenuEl);
-// 361.1 *****part 1
+// 316.1 *****part 1
 const subMenuEl = document.getElementById("sub-menu");
 console.log(subMenuEl);
-subMenuEl.textContent = subMenuEl.textContent.toUpperCase()
-// // 361.3 *****part 4.1.1
+subMenuEl.textContent = subMenuEl.textContent.toUpperCase();
+// // 316.3 *****part 4.1.1
 const subMenuLinks = subMenuEl.getElementsByTagName("a");
 console.log(subMenuLinks);
 // topMenuEl.querySelector('a')
@@ -43,11 +43,11 @@ console.log(subMenuLinks);
 // document.links;
 // document.querySelectorAll('#top-menu a');
 
-// 361.1 *****part 1
+// 316.1 *****part 1
 mainEl.style.backgroundColor = "var(--main-bg)";
-const h1 = document.createElement("h1")
-h1.innerText = "DOM Manupulation"
-mainEl.appendChild(h1)
+const h1 = document.createElement("h1");
+h1.innerText = "DOM Manupulation";
+mainEl.appendChild(h1);
 mainEl.classList.add("flex-ctr");
 
 // 316.1 *****part 2
@@ -82,6 +82,7 @@ function clickedOn(evt) {
   if (clickedLink.localName !== "a") return;
   if (clickedLink.classList.contains("active")) {
     clickedLink.classList.remove("active");
+    subMenuEl.style.top = "0";
   } else {
     // look more at family nodes
     for (i = 0; i < clickedLink.parentNode.childNodes.length; i++) {
@@ -89,15 +90,16 @@ function clickedOn(evt) {
     }
     clickedLink.classList.add("active");
     for (i = 0; i < menuLinks.length; i++) {
-      if (menuLinks[i].text === clickedLink.innerHTML) {// innerHTML
-        subMenuEl.innerHTML =""
-        menuLinks[i].subLinks.forEach(link=>{
-        const a = document.createElement('a')
-        a.setAttribute('href', link.href)
-        a.textContent= link.text
-        subMenuEl.appendChild(a);
-      })
-        if ('subLinks' in menuLinks[i]) {
+      if (menuLinks[i].text === clickedLink.innerHTML) {
+        // innerHTML
+        subMenuEl.innerHTML = "";
+        menuLinks[i].subLinks.forEach((link) => {
+          const a = document.createElement("a");
+          a.setAttribute("href", link.href);
+          a.textContent = link.text;
+          subMenuEl.appendChild(a);
+        });
+        if ("subLinks" in menuLinks[i]) {
           subMenuEl.style.top = "100%";
         } else {
           subMenuEl.style.top = "0";
@@ -105,20 +107,20 @@ function clickedOn(evt) {
       }
     }
   }
-subMenuEl.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  if (!evt.target.matches("a")) {
-    return;
-  }
-  subMenuEl.style.top = "0";
-  topMenuLinks.forEach((link) => {
-    link.classList.remove("active");
+  subMenuEl.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    if (!evt.target.matches("a")) {
+      return;
+    }
+    subMenuEl.style.top = "0";
+    topMenuLinks.forEach((link) => {
+      link.classList.remove("active");
+    });
+    // evt.target.textContent = evt.target.textContent.toUpperCase
+    // console.log(evt.target.textContent);
+    h1.textContent = `${evt.target.textContent}`;
+    h1.textContent = h1.textContent.toUpperCase();
   });
-  // evt.target.textContent = evt.target.textContent.toUpperCase
-  // console.log(evt.target.textContent);
-  h1.textContent = `${evt.target.textContent}`
-  h1.textContent = h1.textContent.toUpperCase()
-});
 }
 // allLinks.forEach(link => link.classList.remove("active"));
 
